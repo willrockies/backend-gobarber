d137ed6ebc6fd8fff4b57900ba98df547e9258fb
W. Alves da Silva <w.alves.da.silva@avanade.com>
2020-01-29 23:22:46 -0300

    commit iniciando backend

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,15 @@
 import { Router } from 'express';
-
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Wilson Alves',
-    email: 'w.alves.da.silva@avanade.com',
-    password_hash: '123456789',
-  });
-  // return res.json({ message: 'Hello Wilson!' });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
 export default routes;
