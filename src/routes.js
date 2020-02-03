1fa51d4cdf512fe6dbe1c93b70995df2925dc80c
Wilson Alves da Silva <w.alves.da.silva@avanade.com>
2020-02-02 22:39:25 -0300

    listando agedamento do usuário

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
